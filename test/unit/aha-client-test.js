029621c4db29d881a38d0e028bb199c91697fa8f
Alex Gandy <alexgandy@gmail.com>
2016-04-27 21:57:12 -0400

    initial commit

---
+++ test/unit/aha-client-test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var _ = require('lodash');
+var expect = require('chai').expect;
+
+var AhaClient = require('../../lib/AhaClient');
+
+describe('lib/index.js', function() {
+   context('constructor', function() {
+       it('should correctly build a client', function() {
+           let aha = new AhaClient('user', 'pass', 'sub');
+
+           expect(aha.baseUrl).to.eql('https://sub.aha.io/api/v1');
+           expect(aha.auth.user).to.eql('user');
+           expect(aha.auth.pass).to.eql('pass');
+       });
+
+       it('should correctly handle non-subdomain clients', function() {
+           let aha = new AhaClient('user', 'pass', 'sub', {useSubdomain: false});
+
+           expect(aha.baseUrl).to.eql('https://secure.aha.io/api/v1');
+           expect(_.isEmpty(aha.requestDefaultHeaders)).to.eql(false);
+           expect(aha.requestDefaultHeaders['X-AHA-ACCOUNT']).to.eql('sub');
+       });
+   })
+});
